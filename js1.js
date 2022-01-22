049baccebf99aa3c5d6f68415c1cc2b0e0f4e9ac
yash mundada <yashmundra@YASHs-MacBook-Air.local>
2022-01-22 23:12:38 +0530

    first commit

---
+++ js1.js
@@ -0,0 +1,61 @@
+// function validation(){
+    
+//     var name1= document.getElementById("name1").value;
+// var bitsid= document.getElementById("bitsid").value;
+// var password= document.getElementById("password").value;
+// var confirm_password= document.getElementById("confirm_password").value;
+// var email= document.getElementById("email").value;
+// var phone= document.getElementById("phone").value;
+
+// var namecheck= /^[A-Za-z. ]{3,20}$/ ;
+// var bitsidcheck= /^20[0-9]{2}[A-Z][0-9][A-Z]{2}[0-9]{4}P$/;
+// var passwordcheck= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+// var emailcheck = /^f20[0-9]{6}@pilani.bits-pilani.ac.in$/;
+// var phonecheck = /^[789][0-9]{9}$/ ; 
+// // Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+// // (?=.*) at least one of...
+
+// if(namecheck.test(name1)){
+//     document.getElementById('nameerror').innerHTML=" ";
+// }else{
+//     document.getElementById('nameerror').innerHTML="**Name too long**"
+// }
+
+// if(bitsidcheck.test(bitsid)){
+//     document.getElementById('iderror').innerHTML=" ";
+// }else{
+//     document.getElementById('iderror').innerHTML="**Invalid BITS ID**";
+//     return false;
+// }
+
+// if(passwordcheck.test(password)){
+//     document.getElementById('passworderror').innerHTML=" ";
+// }else{
+//     document.getElementById('passworderror').innerHTML="**Password must have at least eight characters, at least one uppercase letter, one lowercase letter, one number and one special character **";
+//     return false;
+// }
+
+// if(emailcheck.test(email )){
+//     document.getElementById('emailerror').innerHTML=" ";
+// }else{
+//     document.getElementById('emailerror').innerHTML="**Invalid Email ID**";
+//     return false;
+// }
+
+// if(phonecheck.test(phone)){
+//     document.getElementById('phoneerror').innerHTML=" ";
+// }else{
+//     document.getElementById('phoneerror').innerHTML="**Invalid Mobile Number **";
+//     return false;
+// }
+
+// if(password.match(confirm_password)){
+//     document.getElementById('confirmpassworderror').innerHTML=" ";
+// }else{
+//     document.getElementById('confirmpassworderror').innerHTML="**Password does not match**";
+//     return false;
+// }
+
+
+// }
+
